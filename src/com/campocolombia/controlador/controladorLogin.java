00186d36bbf6016cc3452ea6381da6eb16f74afa
JonasTronco <infanpearl@gmail.com>
2019-11-10 02:19:05 -0500

    Se finaliza de Login

--- src/com/campocolombia/controlador/controladorLogin.java
+++ src/com/campocolombia/controlador/controladorLogin.java
@@ -8,23 +8,27 @@ package com.campocolombia.controlador;
 import com.campocolombia.modelo.user.User;
 import com.campocolombia.modelo.user.consultaUser;
 import com.campocolombia.vista.login;
+import com.campocolombia.vista.principal;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author Jonattan
  */
-public abstract class controladorLogin implements ActionListener{
+public  class controladorLogin implements ActionListener{
     
     private User User;
     private consultaUser consultaUser;
     private login frmLogin;
+    private principal frmPrincipal;
 
-    public controladorLogin(User User, consultaUser consultaUser, login login) {
+    public controladorLogin(User User, consultaUser consultaUser, login login, principal principal) {
         this.User = User;
         this.consultaUser = consultaUser;
-        this.frmLogin = login;       
+        this.frmLogin = login;    
+        this.frmPrincipal = principal;
         this.frmLogin.botonIngresar.addActionListener(this);
     }
     
@@ -35,13 +39,34 @@ public abstract class controladorLogin implements ActionListener{
             User.setNick(frmLogin.textUserName.getText());
             User.setPassword(frmLogin.textUserPassword.getText());
             User.setRol(frmLogin.selectRol.getItemAt(0));
+            try {
+                
+                if (consultaUser.consultaUser(User)) {
+                frmPrincipal.setVisible(true);
+                frmLogin.setVisible(false);
+                limpiar();
+            }else{
+                JOptionPane.showMessageDialog(null, "Los datos no son correctos");
+                limpiar();
+            }
+                
+            } catch (Exception w) {
+                System.err.println(w + " Login" );
+            }
+            
         }
         
     
     }
     
+    public void limpiar(){
+        
+        //frmLogin.textUserName.setText(null);
+        frmLogin.textUserPassword.setText(null);
+        
+    }
     
-    
+      
     
     
     
